27778ab2c6898c08ccf7b5facd93316879d4e0d2
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-02-21 10:44:19 +0100

    FIRST COMMIT

---
+++ Project II Game/Motor2D/j1Player.cpp
@@ -0,0 +1,83 @@
+#include "p2Defs.h"
+#include "j1Scene.h"
+#include "j1App.h"
+#include "p2Log.h"
+#include "j1Textures.h"
+#include "j1Input.h"
+#include "j1Audio.h"
+#include "j1Render.h"
+#include "j1Player.h"
+#include "j1Scene.h"
+
+
+
+j1Player::j1Player() : j1Module()
+{
+	name.create("player");
+}
+
+
+j1Player::~j1Player()
+{
+
+}
+
+bool j1Player::Awake(pugi::xml_node& config)
+{
+	bool ret = true;
+
+	folder.create(config.child("folder").child_value());
+
+
+	node = config;
+	return ret;
+}
+
+
+bool j1Player::Start()
+{
+	bool ret = true;
+
+	Tex_Player = App->tex->Load("");
+
+	return ret;
+}
+
+bool j1Player::PreUpdate()
+{
+
+	return true;
+}
+
+bool j1Player::Update(float dt)
+{
+
+	App->render->DrawQuad({10,10,1000,1000}, 200, 200, 50, 80);
+	return true;
+}
+
+
+
+bool j1Player::Save(pugi::xml_node& data) 
+{
+	//PLAYER POSITION
+	LOG("Loading player state");
+	mouse_position.x = data.child("position").attribute("X").as_int();
+	mouse_position.y = data.child("position").attribute("Y").as_int();
+	return true;
+}
+
+bool j1Player::Load(pugi::xml_node& data)
+{
+	//PLAYER POSITION
+	LOG("Loading player state");
+	mouse_position.x = data.child("position").attribute("X").as_int();
+	mouse_position.y = data.child("position").attribute("Y").as_int();
+	return true;
+}
+
+bool j1Player::CleanUp() {
+
+	return true;
+
+}
---
+++ Project II Game/Motor2D/j1Player.h
@@ -0,0 +1,44 @@
+#ifndef __ModulePlayer_H__
+#define __ModulePlayer_H__
+
+#include "j1Module.h"
+#include "p2Point.h"
+#include "p2Log.h"
+#include "SDL/include/SDL_rect.h"
+
+struct SDL_Texture;
+
+enum states {
+	DRAGGING,
+	RELEASE
+};
+
+
+class j1Player : public j1Module
+{
+public:
+	j1Player();
+	~j1Player();
+
+	bool Awake(pugi::xml_node&);
+	bool Start();
+	bool PreUpdate();
+	bool Update(float dt);
+	bool CleanUp();
+
+	//LOADING AND SAVING OPTIONS
+	bool Save(pugi::xml_node& data);
+	bool Load(pugi::xml_node& data);	
+	
+public:
+
+	iPoint mouse_position; 
+	SDL_Texture* Tex_Player;
+	states state;	
+
+	p2SString	folder;
+	pugi::xml_node	node;
+	p2SString file;
+};
+
+#endif
