549ea2c5fe454a04493e2c5e4970dd3b08d62d8d
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-03-02 12:07:20 +0100

    position changed

--- Project II Game/Motor2D/j1InGameUI.cpp
+++ Project II Game/Motor2D/j1InGameUI.cpp
@@ -58,7 +58,7 @@ void j1InGameUI::Add_UI()
 {
 	menu.Menu_button = App->gui->ADD_ELEMENT(GUItype::GUI_BUTTON, nullptr, { 1400,20 }, { 0,0 }, true, true, { 0,0,100,50 }, "OPTIONS", this);
 	menu.Return_button = App->gui->ADD_ELEMENT(GUItype::GUI_BUTTON, nullptr, { MiddleScreenW,MiddleScreenH }, { 00,0 }, true, false, { 0, 0,130,22 }, "RETURN", this);
-	menu.Title = App->gui->ADD_ELEMENT(GUItype::GUI_BUTTON, nullptr, { MiddleScreenW,MiddleScreenH-200}, { 0,0 }, false, false, { 0, 0,139,27 }, "MENU", this, false, false, SCROLL_TYPE::SCROLL_NONE, true);
+	menu.Title = App->gui->ADD_ELEMENT(GUItype::GUI_BUTTON, nullptr, { MiddleScreenW - 20,MiddleScreenH-140}, { 0,0 }, false, false, { 0, 0,175,47 }, "MENU", this, false, false, SCROLL_TYPE::SCROLL_NONE, true);
 	menu.Resume_button = App->gui->ADD_ELEMENT(GUItype::GUI_BUTTON, nullptr, { MiddleScreenW,MiddleScreenH + 30 }, { 0,0 }, true, false, { 0, 0,130,22 }, "RESUME", this);
 	menu.Exit_button = App->gui->ADD_ELEMENT(GUItype::GUI_BUTTON, nullptr, { MiddleScreenW,MiddleScreenH +60 }, { 0,0 }, true, false, { 0, 0,130,22 }, "EXIT", this);
 	menu.Save = App->gui->ADD_ELEMENT(GUItype::GUI_BUTTON, nullptr, { MiddleScreenW,MiddleScreenH +90 }, { 0,0 }, true, false, { 0, 0,130,22 }, "SAVE", this);
