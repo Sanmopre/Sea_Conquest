8541d17af468ad187bd470771c0ba47e9df5f5f0
Leukino <miquelqg99@gmail.com>
2020-04-07 04:58:47 +0200

    now new map gets all textures from patterns

---
+++ Project II Game/Game/textures/pattern packs tiled/boat.tsx
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="boat" tilewidth="32" tileheight="32" tilecount="24" columns="6">
+ <properties>
+  <property name="level" type="int" value="1"/>
+  <property name="name" value="boat"/>
+ </properties>
+ <image source="../Pipoya Vehicle &amp; Ship Sprites/8-directional/pipo-boat.png" width="192" height="128"/>
+ <tile id="0" type="animation_eight_south">
+  <animation>
+   <frame tileid="0" duration="300"/>
+   <frame tileid="1" duration="300"/>
+   <frame tileid="2" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="3" type="animation_eight_southwest">
+  <animation>
+   <frame tileid="3" duration="300"/>
+   <frame tileid="4" duration="300"/>
+   <frame tileid="5" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="6" type="animation_eight_west">
+  <animation>
+   <frame tileid="6" duration="300"/>
+   <frame tileid="7" duration="300"/>
+   <frame tileid="8" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="9" type="animation_eight_southeast">
+  <animation>
+   <frame tileid="9" duration="300"/>
+   <frame tileid="10" duration="300"/>
+   <frame tileid="11" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="12" type="animation_eight_east">
+  <animation>
+   <frame tileid="12" duration="300"/>
+   <frame tileid="13" duration="300"/>
+   <frame tileid="14" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="15" type="animation_eight_northwest">
+  <animation>
+   <frame tileid="15" duration="300"/>
+   <frame tileid="16" duration="300"/>
+   <frame tileid="17" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="18" type="animation_eight_north">
+  <animation>
+   <frame tileid="18" duration="300"/>
+   <frame tileid="19" duration="300"/>
+   <frame tileid="20" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="21" type="animation_eight_northeast">
+  <animation>
+   <frame tileid="21" duration="300"/>
+   <frame tileid="22" duration="300"/>
+   <frame tileid="23" duration="300"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ Project II Game/Game/textures/pattern packs tiled/galleon.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="galleon" tilewidth="32" tileheight="32" tilecount="24" columns="6">
+ <properties>
+  <property name="level" value="2"/>
+  <property name="name" value="galleon"/>
+ </properties>
+ <image source="../Pipoya Vehicle &amp; Ship Sprites/8-directional/pipo-ship.png" width="192" height="128"/>
+</tileset>
