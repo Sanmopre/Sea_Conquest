bd8be93265f4c7c30376715e49aa9c58f3a4a620
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-04-18 11:13:57 +0200

    Minimap Implemented (not final version)
    This minimap uses a texture and does not use the xml information form the map to draw the minimap (bad)

--- Project II Game/Motor2D/j1Minimap.cpp
+++ Project II Game/Motor2D/j1Minimap.cpp
@@ -5,6 +5,7 @@
 #include "j1Textures.h"
 #include "j1Input.h"
 #include "j1Map.h"
+#include "j1SceneManager.h"
 
 #include "p2Log.h"
 #include <list>
@@ -19,46 +20,33 @@ j1Minimap::~j1Minimap() {}
 bool j1Minimap::Awake(pugi::xml_node & config)
 {
 	size = config.attribute("size").as_int(1);
-	position.x= config.attribute("position.x").as_int(50);
-	position.y= config.attribute("position.y").as_int(50);
+	position.x= config.attribute("position.x").as_int(1020);
+	position.y= config.attribute("position.y").as_int(560);
 
 	return true;
 }
 
 bool j1Minimap::Start()
 {
-	display = true;
-	Load();
-
+	minimap_tex = App->tex->Load("textures/minimap.png");
+	minimap_camera = App->tex->Load("textures/minimap_camera.png");
 	return true;
 }
 
 
 bool j1Minimap::Update(float dt)
 {	
-	if (App->input->GetMouseButtonDown(SDL_BUTTON_LEFT) == KEY_REPEAT)
-	{
-		int map_x, map_y;
-		
-		if (MinimapCoords(map_x, map_y))
-		{
-			App->render->camera.x = -map_x + App->win->width / 2;
-			App->render->camera.y = -map_y + App->win->height / 2;
-		}
+	if (App->scenemanager->In_Logo_Scene != true && App->scenemanager->In_Main_Menu != true) {
+		App->render->AddBlitEvent(5, minimap_tex, position.x - App->render->camera.x, position.y - App->render->camera.y, rect, false, true, 0u, 0u, 0u, 255, true);
+		DrawCamera();
 	}
-
+	
 	return true;
 }
 
 bool j1Minimap::PostUpdate()
 {	
-	if (display) {
-		App->render->AddBlitEvent(5,minimap_tex, position.x, position.y, rect);
 
-		MinimapBorders();
-		DrawCamera();
-	}
-	
 	return true;
 }
 
@@ -69,116 +57,17 @@ bool j1Minimap::CleanUp()
 	return true;
 }
 
-void j1Minimap::Load() {
-	if (App->map->active)
-	{
-	
-		minimap_scale = (float)size / (float)App->map->data.tile_height;
 
-		
-		float map_width = App->map->data.width * App->map->data.tile_width;
-		minimap_width = map_width * minimap_scale;
-		float map_height = App->map->data.height * App->map->data.tile_height;
-		minimap_height = map_height * minimap_scale;
-
-		minimap_tex = SDL_CreateTexture(App->render->renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, minimap_width, minimap_height);
-		SDL_SetRenderTarget(App->render->renderer, minimap_tex);
-	}
-	DrawMinimap();
-
-	SDL_SetRenderTarget(App->render->renderer, NULL);
-	SDL_SetTextureBlendMode(minimap_tex, SDL_BLENDMODE_BLEND);
-}
-
-bool j1Minimap::MinimapCoords(int& map_x, int& map_y)
-{
-	int mouse_x, mouse_y;
-	App->input->GetMousePosition(mouse_x, mouse_y);
-
-	//if we click inside the minimap
-	if (mouse_x >= position.x && mouse_x <= minimap_width +position.x	&&	mouse_y >= position.y && mouse_y <= minimap_height+position.y) 
-	{
-		 if (App->map->data.type == MAPTYPE_ISOMETRIC) {
-			map_x = ((mouse_x - position.x - minimap_width / 2) / minimap_scale);
-			map_y = ((mouse_y - position.y) / minimap_scale);
-		}
-	}
-
-	else
-		return false;
-
-	return true;
-}
 
 void j1Minimap::DrawMinimap()
 {
 	
-	p2List_item<MapLayer*>* item = App->map->data.layers.start;
-
-	for (; item != NULL; item = item->next)
-	{
-		MapLayer* layer = item->data;
-		if (layer->properties.Get("Nodraw") != 0)
-			continue;
-
-		for (int y = 0; y < App->map->data.height; ++y)
-		{
-			for (int x = 0; x < App->map->data.width; ++x)
-			{
-				int tile_id = layer->Get(x, y);
-				if (tile_id > 0)
-				{
-					TileSet* tileset = App->map->GetTilesetFromTileId(tile_id);
-
-					SDL_Rect r = tileset->GetTileRect(tile_id);
-					iPoint pos = App->map->MapToWorld<iPoint>(x, y);
-
-					pos.x *= minimap_scale;
-					pos.y *= minimap_scale;
-
-					App->render->AddBlitEvent(4, tileset->texture, pos.x, pos.y, r);
-
-				}
-			}
-		}
-	}
 }
 
 void j1Minimap::DrawCamera()
 {
-
-	if (App->map->data.type == MAPTYPE_ISOMETRIC) {
-		SDL_Rect map_camera = { -App->render->camera.x * minimap_scale + minimap_width / 2 + position.x,-App->render->camera.y * minimap_scale + position.y,App->render->camera.w * minimap_scale,App->render->camera.h * minimap_scale };
-		App->render->DrawQuad(map_camera, 255, 255, 0, 255, false, false);
-	}
+	App->render->AddBlitEvent(6, minimap_camera, 120 + position.x - App->render->camera.x/App->win->scale - (App->render->camera.x / 50) / App->win->scale, position.y - App->render->camera.y / App->win->scale - (App->render->camera.y / 50) / App->win->scale, cameraminimap, false);
 }
 
-void j1Minimap::MinimapBorders()
-{
-	int isotile_x_offset = App->map->data.tile_width / 2 * minimap_scale;
-	int isotile_y_offset = App->map->data.tile_height / 2 * minimap_scale;
 
- if (App->map->data.type == MAPTYPE_ISOMETRIC){
-		App->render->DrawLine(isotile_x_offset + position.x, minimap_height / 2 + position.y + isotile_y_offset, minimap_width / 2 + isotile_x_offset + position.x, isotile_y_offset + position.y, 255, 255, 255, 255, false);
-		App->render->DrawLine(minimap_width + isotile_x_offset + position.x, minimap_height / 2 + isotile_y_offset + position.y, minimap_width / 2 + isotile_x_offset + position.x, isotile_y_offset + position.y, 255, 255, 255, 255, false);
-		App->render->DrawLine(minimap_width + isotile_x_offset + position.x, minimap_height / 2 + isotile_y_offset + position.y, minimap_width / 2 + isotile_x_offset + position.x, minimap_height + isotile_y_offset + position.y, 255, 255, 255, 255, false);
-		App->render->DrawLine(isotile_x_offset + position.x, minimap_height / 2 + isotile_y_offset + position.y, minimap_width / 2 + isotile_x_offset + position.x, minimap_height + isotile_y_offset + position.y, 255, 255, 255, 255, false);
-	}
 
-}
-
-void j1Minimap::Scale() {
-	if (size < (App->map->data.tile_height / 2)) {
-		size += 1;
-		CleanUp();
-		Load();
-	}
-}
-
-void j1Minimap::Descale() {
-	if (size > 1) {
-		size -= 1;
-		CleanUp();
-		Load();
-	}
-}
