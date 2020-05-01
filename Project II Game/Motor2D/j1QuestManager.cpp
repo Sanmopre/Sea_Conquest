f97d6b0054c3f0757441a9868a2dcb0c18d13fe2
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-05-01 15:56:48 +0200

    ADDED QUEST
    -K to set build boats quest
    -L to set no quest

--- Project II Game/Motor2D/j1QuestManager.cpp
+++ Project II Game/Motor2D/j1QuestManager.cpp
@@ -10,6 +10,8 @@
 #include "j1GUI.h"
 #include "j1Audio.h"
 #include "j1InGameUI.h"
+#include "j1Fonts.h"
+#include "j1Input.h"
 #include "SDL\include\SDL.h"
 
 
@@ -37,17 +39,79 @@ bool j1QuestManager::Start()
 	manager.button = App->gui->AddElement(GUItype::GUI_BUTTON, nullptr, {950 + 180,15}, { 0,0 }, true, false, { 0,0,30,30 }, "", this, false, false, SCROLL_TYPE::SCROLL_NONE, true, TEXTURE::QUEST_CLOSE);
 	manager.image_close = App->gui->AddElement(GUItype::GUI_IMAGE, nullptr, { 950 ,15 }, { 0,0 }, true, false, { 0, 0,180,30 }, "", this, false, false, SCROLL_TYPE::SCROLL_NONE, true, TEXTURE::QUEST_IMAGE_CLOSE);
 	manager.image_open = App->gui->AddElement(GUItype::GUI_IMAGE, nullptr, { 950 ,15 + 30 }, { 0,0 }, true, false, { 0, 0,180,180 }, "", this, false, false, SCROLL_TYPE::SCROLL_NONE, true, TEXTURE::QUEST_IMAGE_OPEN);
+	manager.current = App->gui->AddElement(GUItype::GUI_LABEL, nullptr, { 965, 110 }, { 0,0 }, true, true, { 0,0,40,40 }, "CURRENT :", this, false, false, SCROLL_TYPE::SCROLL_NONE, true);
+	manager.total = App->gui->AddElement(GUItype::GUI_LABEL, nullptr, { 965, 150 }, { 0,0 }, true, true, { 0,0,40,40 }, "OBJECTIVE :", this, false, false, SCROLL_TYPE::SCROLL_NONE, true);
+	manager.reward = App->gui->AddElement(GUItype::GUI_LABEL, nullptr, { 965, 190 }, { 0,0 }, true, true, { 0,0,40,40 }, "REWARD :", this, false, false, SCROLL_TYPE::SCROLL_NONE, true);
+	manager.no_quest = App->gui->AddElement(GUItype::GUI_LABEL, nullptr, { 985, 80 }, { 0,0 }, true, true, { 0,0,40,40 }, "NO QUEST ACTIVE!", this, false, false, SCROLL_TYPE::SCROLL_NONE, true);
+	quest_explanation_build_boat = App->gui->AddElement(GUItype::GUI_LABEL, nullptr, { 995, 70 }, { 0,0 }, true, true, { 0,0,40,40 }, "BUILD 10 BOATS", this, false, false, SCROLL_TYPE::SCROLL_NONE, true);
 
-	current_quest = Set_Quest(QUEST::BUILD_10_BOATS);
+
 	return true;
 }
 
 bool j1QuestManager::Update(float dt)
 {
-	switch (current_quest) {
-	case QUEST::BUILD_10_BOATS:
-		break;
+
+	//MANAGE QUEST NUMBERS
+	sprintf_s(main_quest.total_t, 10, "%7d", main_quest.total);
+	sprintf_s(main_quest.reward_t, 10, "%7d", main_quest.reward);
+	sprintf_s(main_quest.current_t, 10, "%7d", main_quest.current);
+
+	//MANAGE NUMBERS WHEN QUEST I AVALIBLE AND OPEN
+	if (quest_manager_open == true && quest_activate == true) {
+		manager.current->enabled = true;
+		manager.reward->enabled = true;
+		manager.total->enabled = true;
+		quest_explanation_build_boat->enabled = true;
+		App->fonts->BlitText(1005, 110, 1, main_quest.current_t);
+		App->fonts->BlitText(1005, 150, 1, main_quest.total_t);
+		App->fonts->BlitText(1005, 190, 1, main_quest.reward_t);
+
+
+		//DEACTIVATE NO QUEST MESSAGE
+		manager.no_quest->enabled = false;
 	}
+
+	//MANAGES WHEN EVERYTHING IS CLOSED AND NO QUEST
+	if (quest_manager_open == false && quest_activate == false) {
+		manager.current->enabled = false;
+		manager.reward->enabled = false;
+		manager.total->enabled = false;
+		manager.no_quest->enabled = false;
+		quest_explanation_build_boat->enabled = false;
+	}
+
+	//MANAGES WHEN OPEN AND NO QUEST
+	if (quest_manager_open == true && quest_activate == false) {
+		manager.current->enabled = false;
+		manager.reward->enabled = false;
+		manager.total->enabled = false;
+		manager.no_quest->enabled = true;
+		quest_explanation_build_boat->enabled = false;
+	}
+
+
+
+	//MANAGES WHEN IS CLOSED ALWAYS
+	if (quest_manager_open == false) {
+		manager.current->enabled = false;
+		manager.reward->enabled = false;
+		manager.total->enabled = false;
+		manager.no_quest->enabled = false;
+		quest_explanation_build_boat->enabled = false;
+	}
+
+	////////////////////////////////////////////////////////////////
+	if (App->input->GetKey(SDL_SCANCODE_K) == KEY_DOWN)
+	{
+		current_quest = Set_Quest(QUEST::BUILD_10_BOATS);
+	}
+
+	if (App->input->GetKey(SDL_SCANCODE_L) == KEY_DOWN)
+	{
+		current_quest = Set_Quest(QUEST::NONE);
+	}
+	////////////////////////////////////////////////////////////////
 	return true;
 }
 
@@ -64,32 +128,58 @@ QUEST j1QuestManager::Set_Quest(QUEST quest)
 		main_quest.total = 10;
 		main_quest.reward = 10;
 		main_quest.current = 0;
+		Select_Quest_Text(quest);
+		quest_activate = true;
 		return QUEST::BUILD_10_BOATS;
 	break;
+
+	case QUEST::NONE:
+		main_quest.total = 0;
+		main_quest.reward = 0;
+		main_quest.current = 0;
+		Select_Quest_Text(quest);
+		quest_activate = false;
+		return QUEST::NONE;
+		break;
 	}
 
-
-	
-	return QUEST::NONE;
 }
 
-void j1QuestManager::Restart_Quest(QUEST quest)
-{
-}
-
-void j1QuestManager::Cancel_Quest(QUEST quest)
-{
-}
 
 void j1QuestManager::Finish_Quest(QUEST quest)
 {
 }
+
+void j1QuestManager::Select_Quest_Text(QUEST quest)
+{
+	
+	switch (quest)
+	{
+	case QUEST::BUILD_10_BOATS:
+		quest_explanation_build_boat->enabled = true;
+		break;
+
+	case QUEST::NONE:
+		quest_explanation_build_boat->enabled = false;
+		break;
+	}
+
+}
+
 
 void j1QuestManager::Close_Quest_Manager()
 {
 	manager.button->enabled = false;
 	manager.image_close->enabled = false;
 	manager.image_open->enabled = false;
+	manager.current->enabled = false;
+	manager.reward->enabled = false;
+	manager.total->enabled = false;
+	manager.no_quest->enabled = false;
+	quest_explanation_build_boat->enabled = false;
+
+
+
 	quest_manager_open = false;
 }
 
@@ -110,10 +200,17 @@ void j1QuestManager::GUI_Event_Manager(GUI_Event type, j1Element* element)
 		App->audio->PlayFx(App->audio->ui_wood_hit);
 		if (element == manager.button) {
 			manager.image_open->enabled = !manager.image_open->enabled;
-			if(manager.image_open->enabled == true)
+			if (manager.image_open->enabled == true && quest_activate == true) {
+				manager.no_quest->enabled = false;
 				quest_manager_open = true;
-			else 
+			}
+			else  if (manager.image_open->enabled == true && quest_activate == false){
+				manager.no_quest->enabled = true;
+				quest_manager_open = true;
+			}
+			else {
 				quest_manager_open = false;
+			}
 		}
 	}
 	break;
--- Project II Game/Motor2D/j1QuestManager.h
+++ Project II Game/Motor2D/j1QuestManager.h
@@ -15,12 +15,12 @@ NONE
 };
 
 struct Quest {
-	int total;
-	char total_t[5];
-	int current;
-	char current_t[5];
-	int reward;
-	char reward_t[5];
+	int total = 0;
+	char total_t[10];
+	int current = 0;
+	char current_t[10];
+	int reward = 0;
+	char reward_t[10];
 
 	j1Element* text;
 	j1Element* image;
@@ -31,6 +31,10 @@ struct QuestManagerUI {
 	j1Element* image_open;
 	j1Element* image_close;
 	j1Element* button;
+	j1Element* current;
+	j1Element* total;
+	j1Element* reward;
+	j1Element* no_quest;
 };
 
 class j1QuestManager : public j1Module
@@ -50,17 +54,20 @@ public:
 	void Restart_Quest(QUEST quest = QUEST::NONE);
 	void Cancel_Quest(QUEST quest = QUEST::NONE);
 	void Finish_Quest(QUEST quest = QUEST::NONE);
-
+	void Select_Quest_Text(QUEST quest = QUEST::NONE);
 
 	void Close_Quest_Manager();
 	void Open_Quest_Manager();
 	void GUI_Event_Manager(GUI_Event type, j1Element* element);
 public:
 	bool quest_manager_open = false;
+	bool quest_activate = false;
 	QUEST current_quest = QUEST::NONE;
 private:
 	QuestManagerUI manager;
 	Quest main_quest;
+
+	j1Element* quest_explanation_build_boat = nullptr;
 };
 
 
